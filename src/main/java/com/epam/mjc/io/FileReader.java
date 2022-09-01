4b8d6fa8472559e69072c50e893ee0a54b127960
Aliaksandr Artsikhovich <art-orient@tut.by>
2022-09-01 18:05:59 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,24 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = null;
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
+            String name = br.readLine().substring(6);
+            Integer age = Integer.parseInt(br.readLine().substring(5));
+            String email = br.readLine().substring(7);
+            Long phone = Long.parseLong(br.readLine().substring(7));
+            profile = new Profile(name, age, email, phone);
+            System.out.println(profile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return profile;
     }
 }
